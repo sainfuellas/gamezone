0e08e46018fabd2caf01c36629cea0076b80e288
sainfuellas <114379981+sainfuellas@users.noreply.github.com>
2022-12-23 12:26:55 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './screens/home';
+import Navigator from './routes/homestack';
+
+
+
+export default function App() {
+    return (
+      <Navigator />
+    );
+}  
